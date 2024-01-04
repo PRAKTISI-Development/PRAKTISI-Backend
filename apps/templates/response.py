a54d30e108d685cb372527af651e06f9126e0f9b
kahyuwesuma <kahyuwesuma@gmail.com>
2024-01-05 00:13:08 +0800

    optimizing payload

--- apps/templates/response.py
+++ apps/templates/response.py
@@ -9,11 +9,3 @@ def response(status_code: int, success: bool, msg: str, data):
     }]
 
     return json.dumps(payload, indent=2)
-
-data = {
-    'nama' : 'Wahyu Kesuma',
-    'nim'  : 2109116068,
-    'ipk'  : 3.92
-}
-
-print(response(200, True, "OK", data))
