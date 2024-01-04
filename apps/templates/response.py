8b131b403e3d5ca0e9303fd0743bd15ac35d1ace
kahyuwesuma <kahyuwesuma@gmail.com>
2024-01-05 00:03:21 +0800

    added the .gitignore

--- apps/templates/response.py
+++ apps/templates/response.py
@@ -1,15 +1,19 @@
 import json
 
-def response(msg: str, data: any, success: bool, status_code: int) -> json:
-    payload = {
-        'success': success,
-        'status_code': status_code,
-        'message': msg,
-        'data': data
-    }
+def response(status_code: int, success: bool, msg: str, data):
+    payload = [{
+        'status_code' : status_code,
+        'success'     : success,
+        'msg'         : msg,
+        'data'        : [data]
+    }]
 
-    return payload
+    return json.dumps(payload, indent=2)
 
+data = {
+    'nama' : 'Wahyu Kesuma',
+    'nim'  : 2109116068,
+    'ipk'  : 3.92
+}
 
-
-
+print(response(200, True, "OK", data))
