de8cd39765156cb6f47b8fdc0d42feb096bd0e21
kahyuwesuma <kahyuwesuma@gmail.com>
2024-01-05 20:18:46 +0800

    auth update

--- apps/services/auth_service.py
+++ apps/services/auth_service.py
@@ -37,15 +37,5 @@ def verify_token(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise credentials_exception
 
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
 def verify_password(plain_password, hashed_password):
     return plain_password == hashed_password
