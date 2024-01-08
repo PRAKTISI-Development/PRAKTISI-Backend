ae1aee1d5b9d78b79c4784e1fe43334024482703
kahyuwesuma <kahyuwesuma@gmail.com>
2024-01-08 11:59:17 +0800

    baikk

--- apps/service/auth_service.py
+++ apps/service/auth_service.py
@@ -6,6 +6,7 @@ from apps.database import get_db
 from apps.models.users import TokenData
 from apps.models.user import User
 from datetime import datetime, timedelta
+from apps.controllers import user_controller
 from dotenv import load_dotenv
 import os
 
@@ -16,10 +17,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
-# Dependency untuk mendapatkan token dari header Authorization
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Fungsi untuk memverifikasi token JWT
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=401,
@@ -40,13 +39,9 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 def get_user(db: Session, username: str):
     return user_controller.get_user_by_nim(db, username)
 
-# Fungsi untuk memverifikasi kata sandi
 def verify_password(plain_password, hashed_password):
-    # Implementasi verifikasi kata sandi sesuai kebutuhan aplikasi Anda
-    # Contoh sederhana: return plain_password == hashed_password
     return True
 
-# Fungsi untuk mendapatkan role (tipe_user) user
 def get_user_role(db: Session, username: str):
     user = get_user(db, username)
     if user:
