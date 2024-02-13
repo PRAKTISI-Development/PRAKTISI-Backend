314e5b7d01d906dd2790a78313069c5a71fb4af0
kahyuwesuma <kahyuwesuma@gmail.com>
2024-02-13 09:11:09 +0800

    env

--- apps/helpers/file_upload.py
+++ apps/helpers/file_upload.py
@@ -1,12 +1,14 @@
 from fastapi import UploadFile
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 def save_uploaded_file(file: UploadFile):
-    upload_folder = "uploads"
-    if not os.path.exists(upload_folder):
-        os.makedirs(upload_folder)
+    SERVER_FOLDER = os.getenv("SERVER_FOLDER")
+    if not os.path.exists(SERVER_FOLDER):
+        os.makedirs(SERVER_FOLDER)
 
-    file_path = os.path.join(upload_folder, file.filename)
+    file_path = os.path.join(SERVER_FOLDER, file.filename)
 
     with open(file_path, "wb") as f:
         f.write(file.file.read())
